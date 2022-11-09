efc8f2b671a604771469e708765c155fe8e3a7a2
Hins Guo <zongzhen@ualberta.ca>
2022-11-08 20:59:51 -0800

    post action

--- app.py
+++ app.py
@@ -62,8 +62,27 @@ def customers():
         first_name = request.form.get("first_name")
         last_name = request.form.get("last_name")
         email = request.form.get("email")
+        if email == None:
+            email = ""
         phone = request.form.get("phone")
+        if phone == None:
+            phone = ""
         mycursor = my_db.cursor()
+        
+        sql1 = "INSERT INTO Customers (first_name, last_name) VALUES (%s, %s)"
+        val1= (first_name,last_name)
+        mycursor.execute(sql1, val1)
+        my_db.commit()
+        
+        mycursor.execute("SELECT customer_id FROM Customers WHERE first_name = %s AND last_name = %s", (first_name, last_name))
+        myresult = mycursor.fetchall()
+        customer_id = myresult[0][0]
+        
+        sql2 = "INSERT INTO CustomerContactInfo (customer_id, email, phone) VALUES (%s, %s, %s)"
+        val2= (customer_id, email,phone)
+        mycursor.execute(sql2, val2)
+        my_db.commit()
+        
         mycursor.execute("SELECT * FROM Customers INNER JOIN CustomerContactInfo ON Customers.customer_id=CustomerContactInfo.customer_id")
         myresult = mycursor.fetchall()
         return render_template("customers.html", myresult=myresult)
